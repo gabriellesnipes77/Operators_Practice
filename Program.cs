fb25890707c0ff0464132e89655ef2d44b7fc58e
BIG DON <BIG DON@MSI>
2024-12-26 15:14:50 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Operators_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            var c = 5;
+
+            Console.WriteLine(a + b); 
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine((float)a /(float) b);
+            Console.WriteLine((a + b) * c);
+
+        }
+    }
+}
